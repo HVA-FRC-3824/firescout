737b8e66561186ecfa2504716936bc1295767001
Weston <weston.agreda@gmail.com>
2022-01-01 23:37:39 -0500

    new look

--- public/js/global.js
+++ public/js/global.js
@@ -1,25 +1,11 @@
 function navDropdown() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    var x = document.getElementById("navbar");
+    if (x.style.overflow === "visible") {
+        x.style.overflow = "hidden";
     } else {
-        x.style.display = "block";
+        x.style.overflow = "visible";
     }
+
 }
 
-function addNav(){
-    document.getElementById("topnav").innerHTML = 
-    "<img src='pics/Logo-Small-Square.png' alt='3824logo' onclick='location.replace('index.html')'>" + 
-    "<div id='myLinks'>" +
-        "<a href='schedule.html'>Match Scouting</a>" +
-        "<a href='pit.html'>Pit Scouting</a>" +
-        "<a href='analytics.html'>Analytics</a>" +
-        "<a href='preview.html'>Match Preview</a>" +
-        "<a href='settings.html'>Settings</a>" +
-        "<a href='login.html' id='loginButton'>Login</a>" +
-    "</div>" +
 
-    "<a href='javascript:void(0);' class='icon' onclick='navDropdown()'>" +
-        "<i class='fa fa-bars'></i>" +
-    "</a>"
-}
