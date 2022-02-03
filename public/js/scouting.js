79c8c122469aeeb6fa1961c5e21e84bc47ff4ed3
weston <weston.agreda@gmail.com>
2022-02-03 13:15:55 -0500

    auto and tele page frontend

--- public/js/scouting.js
+++ public/js/scouting.js
@@ -22,6 +22,79 @@ window.onload = function() {
 }
 
 openPage("Pre");
-console.log("pre");
 
 console.log(localStorage.getItem("robotToScout"));
+
+function getMousePosition(){
+    //get the users mouse position :)
+}
+
+var popupOpen = false;
+var mousePosition;
+function togglePopup(page){
+    //mousePosition = getMousePosition    //TODO make this function :)
+    if(page == "auto"){
+        if(popupOpen){
+            document.getElementById("autoDropdown").style.display = "none";
+            popupOpen = false;
+        }else{
+            //TODO MOVE autoDropdown to the mousePosition
+            document.getElementById("autoDropdown").style.display = "block";
+            popupOpen = true;
+        }
+    }else{
+        if(popupOpen){
+            document.getElementById("teleopDropdown").style.display = "none";
+            popupOpen = false;
+        }else{
+            //TODO MOVE teleDropdown to the mousePosition
+            document.getElementById("teleopDropdown").style.display = "block";
+            popupOpen = true;
+        }
+    }
+}
+
+function launchedCargo(points, autoOrTeleop){
+    if(autoOrTeleop == "teleop"){
+        switch (points) {
+            case 2:
+                //push to where ever the data is being held that one ball was scored during teleop in the upper hub at mousePosition
+                console.log("teleop upper hub");
+                break;
+            case 1:
+                //push to where ever the data is being held that one ball was scored during teleop in the lower hub at mousePosition
+                console.log("teleop lower hub");
+                break;
+            case 0:
+                //push to where ever the data is being held that one ball was missed during teleop at mousePosition
+                console.log("teleop miss");
+                break;
+        }
+    }else{
+        switch (points) {
+            case 4:
+                //push to where ever the data is being held that one ball was scored during auto in the upper hub at mousePosition
+                console.log("auto upper hub");
+                break;
+            case 2:
+                //push to where ever the data is being held that one ball was scored during auto in the lower hub at mousePosition
+                console.log("auto lower hub");
+                break;
+            case 0:
+                //push to where ever the data is being held that one ball was missed during auto at mousePosition
+                console.log("auto miss");
+                break;
+        }
+    }
+}
+
+function pickupCargo(autoOrTeleop){
+    if(autoOrTeleop == "teleop"){
+        //push to where ever the data is being held that one ball was picked up at mousePosition during teleop
+        console.log("teleop pickup");
+    }else{
+        //push to where ever the data is being held that one ball was picked up at mousePosition during auto
+        console.log("auto pickup");
+    }
+}
+
