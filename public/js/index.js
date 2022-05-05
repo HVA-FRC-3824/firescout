23a1424cc18be74239bd129e03da9fa6e2c35155
LukeD <ljd223@gmail.com>
2022-05-05 09:56:58 -0400

    PWA Andriod Support

--- public/js/index.js
+++ public/js/index.js
@@ -33,4 +33,11 @@ var x = setInterval(function() {
 }, 1000);
 
 
-
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
---
+++ public/serviceWorker.js
@@ -0,0 +1,24 @@
+const cache = "all-the-info";
+const assets = [
+  "/",
+  "/index.html",
+  "/index.css",
+  "/index.js",
+  "/logo.png"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
+
